e674b2020d54ce27dde26a4bc1b0f75ab2090790
sauravbhetwal8@gmail.com <saurav12bhetwal>
2024-01-16 11:57:29 +0530

    dsa

--- backend/Routes/Router.js
+++
@@ -1,5 +0,0 @@
-const express=require("express")
-const router=new express.Router()
-router.use(express.json())
-
-module.exports=router;
--- backend/db/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose=require("mongoose")
-
- mongoose.connect("mongodb://127.0.0.1:27017/University").then(()=>{
-    console.log("db successfully connected")
- }).catch((e)=>{
-    console.log("errror occure while connecting")
- })
-    
